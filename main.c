efe97c1d4864cf8a99501cd59b5bd0fbce9ca889
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-10-04 17:23:55 +0900

    04

--- main.c
+++ main.c
@@ -4,18 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int num = 0;
-	 
-	printf("문자열을 입력하세요: \n");
-	while((c = getchar() ) != '\n')//입력 문자가 개행문자가 나올때까지 반복 
-	{
-	    if(c >= '0' && c <= '9')//입력된 글자가 (c) 가 숫자인가
-	       num = num + 1;	// 그렇다면 num에 하나를 센다. 
-	}
+	int sum = 0;
+	int x;
+	int i;
 	
-	printf("숫자의 개수는 %i개 입니다.\n", num);
+	printf("정수를 입력하시오: ");
+	scanf("%d", &x); 
 	
+	for (i=0;i<=x;i++)
+	   sum = sum + i;
+	   
+	printf("더하기 결과는 %i입니다\n", sum);
 	return 0;
 }
 
