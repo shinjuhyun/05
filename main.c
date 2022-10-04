1c21870ac0267e1e9576d59a3ffc66b5e386bbf1
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-10-04 16:41:00 +0900

    02

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
-	printf("정수를 하나 입력하시오: ");
-	scanf("%d",&i);
-	if(i > 0)
-	  printf("양수입니다\n");
-	else if(i < 0)
-	  printf("음수입니다\n"); 
-	else
-	  printf("0입니다\n");
+	int x;
+	
+	printf("정수 하나를 입력하시오: ");
+	scanf("%d",&x);
+	
+	if(x  <0)
+	   x = -1*x;
+	  
+	printf("절댓값은 %i입니다\n", x);
 	 
 	return 0;
 }
