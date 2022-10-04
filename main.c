e6e56192a64f28d5b9fce25563dd4830ba71a2a4
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-10-04 17:34:24 +0900

    05

--- main.c
+++ main.c
@@ -4,17 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sum = 0;
-	int x;
-	int i;
+	int x,y;
+	char op;
+	int result;
 	
-	printf("정수를 입력하시오: ");
-	scanf("%d", &x); 
+	printf("enter the calculation: ");
+	scanf("%d %c %d", &x, &op, &y);
 	
-	for (i=0;i<=x;i++)
-	   sum = sum + i;
-	   
-	printf("더하기 결과는 %i입니다\n", sum);
+	if (op == '+' )
+	    result = x + y;
+	else if (op == '-')
+	    result = x - y;
+	else if (op == '*')
+	    result = x * y;
+	else 
+	    result = x / y;
+	    
+	printf("= %i\n", result);
 	return 0;
 }
 
